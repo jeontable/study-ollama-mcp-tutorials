6f3b96a19c7a362b8b233a74be8b4b10086a2444
jeontable <jeontable@gmail.com>
2026-08-03 16:09:48 +0900

    update library, improve codes in ch06-01

--- ch06/01_tracing_project/main.py
+++ ch06/01_tracing_project/main.py
@@ -2,10 +2,11 @@ import os
 
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_community.vectorstores import FAISS
+from langchain_chroma import Chroma
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_ollama import ChatOllama
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_ollama.embeddings import OllamaEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
@@ -33,13 +34,11 @@ def create_vector_db():
     embeddings = OllamaEmbeddings(model="bge-m3")
 
     # 1-4. 벡터 저장소 구축 (Vector Database)
-    vector_store = FAISS.from_documents(
+    vector_store = Chroma.from_documents(
         documents=splits,
         embedding=embeddings,
+        persist_directory=VECTOR_DB_PATH
     )
-
-    # 1-5. 벡터 DB를 로컬에 저장
-    vector_store.save_local(VECTOR_DB_PATH)
 
     return vector_store
 
@@ -48,11 +47,7 @@ def create_vector_db():
 if os.path.exists(VECTOR_DB_PATH):
     print("기존 벡터 DB를 로드합니다.")
     embeddings = OllamaEmbeddings(model="bge-m3")
-    vector_store = FAISS.load_local(
-        VECTOR_DB_PATH,
-        embeddings,
-        allow_dangerous_deserialization=True,
-    )
+    vector_store = Chroma(persist_directory=VECTOR_DB_PATH, embedding_function=embeddings)
 else:
     print("새로운 벡터 DB를 생성합니다.")
     vector_store = create_vector_db()
@@ -82,6 +77,7 @@ prompt = PromptTemplate.from_template(
 
 # 5. Ollama 초기화
 llm = ChatOllama(model="qwen3:8b")
+#llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0)
 
 # 6. 체인을 생성합니다.
 chain = prompt | llm | StrOutputParser()
